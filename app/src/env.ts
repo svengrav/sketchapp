68e25c05caae58cb9dffe3d76c93d2937642ba18
svengrav <sv@svengrav.com>
2025-12-07 13:28:47 +0000

    v1.0.15

--- app/src/env.ts
+++ app/src/env.ts
@@ -1,6 +1,6 @@
 // App Environment & Constants
 // Version wird durch docker.bash automatisch inkrementiert
 
-export const APP_VERSION = "1.0.14";
+export const APP_VERSION = "1.0.15";
 
 export const GITHUB_URL = "https://github.com/svengrav/sketchapp";

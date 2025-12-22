d141d44d89d70a2a6fa222806a77347b72e98880
svengrav <sv@svengrav.com>
2025-12-22 12:01:47 +0000

    chore: remove outdated deployment and Nginx configuration files

--- tools.bash
+++
@@ -1,4 +0,0 @@
-docker exec nginx-gateway cat /etc/nginx/nginx.conf 2>/dev/null || docker exec nginx-gateway cat /etc/nginx/conf.d/default.conf 2>/dev/null
-
-docker exec nginx-gateway ls -la /etc/nginx/conf.d/
-
